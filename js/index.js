765499d26f6298d494f0b19396921be7ad37772c
ramez-mneizel <ramizbashar15@gmail.com>
2025-11-26 23:02:24 +0300

    snbnm
    smnnmssm

---
+++ js/index.js
@@ -0,0 +1,48 @@
+alert('welcome to js')
+var x1=document.getElementById('navlogo')
+// x1.style.backgroundColor='red'
+// x1.style.borderRadius='50px'
+x1.style.width='300px'
+
+var image=document .getElementsByClassName('imggrid')[0]
+console.log(image)
+image.src='  https://up.yimg.com/ib/th/id/OIP.0Gk7WStwdI8ZbkOc4Kk3eAHaE_?pid=Api&rs=1&c=1&qlt=95&w=180&h=121 '
+image.style.width='250px'
+
+var image=document .getElementsByClassName('imggrid')[1]
+console.log(image)
+image.src='  https://up.yimg.com/ib/th/id/OIP.0Gk7WStwdI8ZbkOc4Kk3eAHaE_?pid=Api&rs=1&c=1&qlt=95&w=180&h=121 '
+image.style.width='200px'
+
+
+var z=10
+if (z===10) {
+console.log('z equal 10')
+}
+
+
+else if (z!=10)
+{
+
+    console.log('z greater or equal 10')
+}
+
+
+else {
+
+    console.log('z not equal 10')
+}
+
+
+if(z>=10) {
+
+
+
+
+
+
+    console.log('z greater or equal 10')
+}
+
+
+

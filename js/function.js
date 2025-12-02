30d42266dd76b3697caad969bf32d5a5655ec909
ramez-mneizel <ramizbashar15@gmail.com>
2025-12-02 17:58:40 +0300

    hjhk

--- js/function.js
+++ js/function.js
@@ -14,33 +14,39 @@
 
 // Headers1.innerHTML='java'
 var body=document.getElementById('tbody')
-// var button=document.getElementById('btn')
+var button=document.getElementById('btn')
 
-//function changetheme()
-
-
-// if(body.style.backgroundcolor=='white')
-// { 
-//     alert('kkkk')
-//     // body.style.backgroundcolor ='black'
-//     // button.innerText='change to white '
-//     //headers1.style.color='white'
-// }
-//else{
-// body.style.backgroundcolor='white'
-// button.innerHTML='change to black'
- //headers1.style.color='black'
+function changetheme(){
 
 
+ if(body.style.backgroundcolor=='white')
+ { 
+   localStorage.setItem('mode','black')
+ body.style.backgroundcolor ='black'
+ button.innerHTML='change to white'   
+//   headers1.style.color='white'
+ }
 
-//var useDate={
+else{
+    localStorage.setItem('mode','white')
+body.style.backgroundcolor='white'
+button.innerHTML='change to black'
+//  headers1.style.color='black'
+}
+}
+var theme=localStorage.getItem('mood')
+body.style.backgroundColor=theme
 
-    // fullname:'ali',
-    // age:20
-//}
+
+var useDate={
+
+ fullname:'ali',
+  age:20
+}
 
 // console.log(usedata['fullname'])
-// console.log(usedata .age)
+
+// console.log(usedata.age)
 
 localStorage.setItem('name','galyah')
 localStorage.setItem('major','computer egonner')
